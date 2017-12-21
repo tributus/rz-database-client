f5b2d8adf7662f54dc51bd6a4fb588c91d74baa3
Anderson Norberto Santos <anderson.norberto@gmail.com>
2017-12-21 03:21:12 -0200

    RZP-12: Refatoração no client postgresql para adaptação ao ES6 e compliance com o padrão ESLINT AirBnB

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "import/no-dynamic-require":"off",
+        "no-bitwise":"off",
+        "prefer-destructuring":"off",
+        "no-confusing-arrow":"off",
+        "max-len":"off",
+        "no-param-reassign":"off",
+        "object-shorthand":"off",
+    }
+};
--- postgres.js
+++ postgres.js
@@ -1,170 +1,145 @@
+const pg = require('pg');
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  getInterface:function(){
-    return function(config){
-      return new (function(config){
-        var $this = this;
-        var connection = config.connection;
-        var logLevel = config.logLevel || "debug"
-        var baseQueryDir = config.queriesFolder || './queries/';
-        var pg = require('pg');
-        var fs = require('fs');
-        var path = require("path");
-        var sqlCache = {};
+  getInterface: () => config => new (() => {
+    const connection = config.connection;
+    const baseQueryDir = config.queriesFolder || './queries/';
+    const sqlCache = {};
 
-        var displayLog = function() {
-          console.log(Array.prototype.slice.call(arguments));
-        }
+    const displayLog = () => {
+      console.log(Array.prototype.slice.call(arguments));
+    };
 
-        var displayError = function(){
-          console.error(Array.prototype.slice.call(arguments));
-        }
+    const displayError = () => {
+      console.error(Array.prototype.slice.call(arguments));
+    };
 
-        var displayWarning = function(){
-          console.warn(Array.prototype.slice.call(arguments));
-        }
+    const displayWarning = () => {
+      console.warn(Array.prototype.slice.call(arguments));
+    };
 
-        
-        var pool = new pg.Pool(connection);
-        pool.on('error', function (err, client) {
-          displayError('idle client error', err.message, err.stack)
-        });
+    const pool = new pg.Pool(connection);
+    pool.on('error', (err, client) => {
+      displayError('idle client error', err.message, err.stack, client);
+    });
 
-        var resolveQuery = function (queryString,callback) {
-          //displayLog("resolving query for ", queryString);
-          if(queryString.substring(0,3)=="fs:"){
-            readFile(path.join(baseQueryDir,queryString.substring(3)),callback);
-          }
-          else{
-            callback(queryString);
-          }
-        };
+    const sanitizeSqlTemplate = (sql) => {
+      return sql; // (sql || "").replace(/(\n)+/g, " ").replace(/(\t)+/g, " ");
+    };
 
-        var readFile = function (file,callback) {
-          //displayLog("retrieving query content for ", file);
-          if (sqlCache[file]) {
-            callback(sqlCache[file]);
+    const readFile = (file, callback) => {
+      if (sqlCache[file]) {
+        callback(sqlCache[file]);
+      } else {
+        fs.readFile(file, 'utf8', (error, data) => {
+          if (!data) {
+            displayWarning('resolucao da query em " + file + " nao retornou conteudo da query');
+            sqlCache[file] = null;
+            callback(null);
           } else {
-            fs.readFile(file, "utf8", function(error, data) {
-              if (!data) {
-                displayLog("resolucao da query em " + file + " nao retornou conteudo da query");
-                sqlCache[file] = null;
-                callback(null);
-              } else {
-                data = sanitizeSqlTemplate(data);
-                sqlCache[file] = data;
-                callback(data);
-              }
-            });
+            data = sanitizeSqlTemplate(data);
+            sqlCache[file] = data;
+            callback(data);
           }
-        };
+        });
+      }
+    };
 
-        var sanitizeSqlTemplate = function(sql) {
-          return sql; // (sql || "").replace(/(\n)+/g, " ").replace(/(\t)+/g, " ");
-        }
+    const resolveQuery = (queryString, callback) => {
+      if (queryString.substring(0, 3) === 'fs:') {
+        readFile(path.join(baseQueryDir, queryString.substring(3)), callback);
+      } else {
+        callback(queryString);
+      }
+    };
 
-        //impl
-        var impl = {
-          getSQLQuery: function (fname,callback) {
-            readFile(baseQueryDir + fname,callback);
-          },
-          findAll: function (queryPath, params, success_callback, error_callback) {
-            success_callback = success_callback || function(){};
-            error_callback = error_callback || function(){};
-
-            resolveQuery(queryPath, function (queryText) {
-              var results = [];
-              pool.connect(function (err, client, done) {
-                if (err) {
-                  displayLog(err);
-                  error_callback({ error: "DATABASE_CONNECTION_ERROR" }, err);
-                }
-                else {
-                  var query = client.query(new pg.Query(queryText, params));
-                  query.on('row', function (row) {
-                    results.push(row);
-                  });
-                  query.on('end', function () {
-                    done();
-                    success_callback(results);
-                  });
-                  query.on('error', function (err) {
-                    done();
-                    error_callback({ error: "GET_DATA_ERROR", query: query },err);
-                    displayError(err);
-                  });
-                }
-
+    // impl
+    const impl = {
+      getSQLQuery: (fname, callback) => {
+        readFile(baseQueryDir + fname, callback);
+      },
+      findAll: (queryPath, params, success_callback, error_callback) => {
+        resolveQuery(queryPath, (queryText) => {
+          const results = [];
+          pool.connect((err, client, done) => {
+            if (err) {
+              displayLog(err);
+              error_callback({ error: 'DATABASE_CONNECTION_ERROR' }, err);
+            } else {
+              const query = client.query(new pg.Query(queryText, params));
+              query.on('row', (row) => {
+                results.push(row);
               });
-            });
-          },
-          findOne: function (queryPath, params, success_callback, error_callback) {
-            success_callback = success_callback || function(){};
-            error_callback = error_callback || function(){};
-            resolveQuery(queryPath, function (queryText) {
-              var result = undefined;
-              pool.connect(function (err, client, done) {
-
-                if (err) {
-                  displayLog(err);
-                  error_callback({ error: "DATABASE_CONNECTION_ERROR" }, err);
-                }
-                else {
-                  var query = client.query(new pg.Query(queryText, params));
-                  query.on('row', function (row) {
-                    result = row;
-                  });
-                  query.on('end', function () {
-                    done();
-                    success_callback(result);
-                  });
-                  query.on('error', function (err) {
-                    displayError("DB_ERROR: " + err);
-                    done();
-                    error_callback({ error: "GET_DATA_ERROR" }, err);
-                  });
-                }
-
+              query.on('end', () => {
+                done();
+                success_callback(results);
               });
-            });
-          },
-          executeNonQuery: function (queryPath, params, success_callback, error_callback) {
-            success_callback = success_callback || function(){};
-            error_callback = error_callback || function(){};
-
-            resolveQuery(queryPath, function (queryText) {
-              pool.connect(function (err, client, done) {
-                if (err) {
-                  displayLog(err);
-                  error_callback({ error: "DATABASE_CONNECTION_ERROR" }, err);
-                }
-                else {
-                  var query = client.query(new pg.Query(queryText, params));
-                  query.on('end', function (result) {
-                    done();
-                    success_callback(result.rowCount);
-                  });
-                  query.on('error', function (err) {
-                    done();
-                    if (err.code == 23505) {
-                      error_callback({ error: "UNIQUE_VIOLATION" }, err);
-                    }
-                    else {
-                      error_callback({ error: "SET_DATA_ERROR" }, err);
-                    }
-                  });
+              query.on('error', (qerr) => {
+                done();
+                error_callback({ error: 'GET_DATA_ERROR', query: query }, qerr);
+                displayError(qerr);
+              });
+            }
+          });
+        });
+      },
+      findOne: (queryPath, params, success_callback, error_callback) => {
+        resolveQuery(queryPath, (queryText) => {
+          let result;
+          pool.connect((err, client, done) => {
+            if (err) {
+              displayLog(err);
+              error_callback({ error: 'DATABASE_CONNECTION_ERROR' }, err);
+            } else {
+              const query = client.query(new pg.Query(queryText, params));
+              query.on('row', (row) => {
+                result = row;
+              });
+              query.on('end', () => {
+                done();
+                success_callback(result);
+              });
+              query.on('error', (qerr) => {
+                displayError(`DB_ERROR: ${qerr}`);
+                done();
+                error_callback({ error: 'GET_DATA_ERROR' }, err);
+              });
+            }
+          });
+        });
+      },
+      executeNonQuery: (queryPath, params, success_callback, error_callback) => {
+        resolveQuery(queryPath, (queryText) => {
+          pool.connect((err, client, done) => {
+            if (err) {
+              displayLog(err);
+              error_callback({ error: 'DATABASE_CONNECTION_ERROR' }, err);
+            } else {
+              const query = client.query(new pg.Query(queryText, params));
+              query.on('end', (result) => {
+                done();
+                success_callback(result.rowCount);
+              });
+              query.on('error', (qerr) => {
+                done();
+                if (err.code === 23505) {
+                  error_callback({ error: 'UNIQUE_VIOLATION' }, qerr);
+                } else {
+                  error_callback({ error: 'SET_DATA_ERROR' }, err);
                 }
               });
-            });
-          }
-        };        
-        //interface
-        this.getSQLQuery = impl.getSQLQuery;
-        this.findAll = impl.findAll;
-        this.findOne = impl.findOne;
-        this.executeNonQuery = impl.executeNonQuery;
+            }
+          });
+        });
+      },
+    };
 
-
-      })(config)
-    }
-  }
-}
+    // interface
+    this.getSQLQuery = impl.getSQLQuery;
+    this.findAll = impl.findAll;
+    this.findOne = impl.findOne;
+    this.executeNonQuery = impl.executeNonQuery;
+  })(config),
+};
