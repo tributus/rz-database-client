1d15f11b750e28300ca040e68b5e87da52a9263b
Anderson Norberto Santos <anderson.norberto@gmail.com>
2017-12-26 00:10:33 -0200

    RZP-12: Refatoração do dbclient

---
+++ STDOut.js
@@ -0,0 +1,93 @@
+module.exports = {
+  initialize: c => new (function STDOut(s) {
+    const settings = s || { logLevel: 'debug' };
+    settings.logLevel = settings.logLevel || 'debug';
+    const logLevels = {
+      debug: 0,
+      info: 1,
+      warning: 2,
+      error: 3,
+      none: 4,
+    };
+
+    const terminalColors = {
+      red: '\x1b[31m%s\x1b[0m',
+      green: '\x1b[32m%s\x1b[0m',
+      yellow: '\x1b[33m%s\x1b[0m',
+      blue: '\x1b[34m%s\x1b[0m',
+      white: '\x1b[37m%s\x1b[0m',
+      magenta: '\x1b[35m%s\x1b[0m',
+      cyan: '\x1b[36m%s\x1b[0m',
+    };
+
+    const getColor = color => (settings.disableColors) ? '' : terminalColors[color];
+
+    const printErrObject = (err) => {
+      console.log(getColor('red'), `\nmessage:\n${err.message}\n\nstack trace:\n${err.stack}\n`); // eslint-disable-line no-console
+      Object.keys(err).forEach((key) => {
+        console.log(getColor('red'), `\n${key}:\n${err[key]}`); // eslint-disable-line no-console
+      });
+    };
+
+    const printObjects = (list) => {
+      list.forEach((element) => {
+        try {
+          if (element instanceof Error) {
+            printErrObject(element);
+          } else {
+            console.log(getColor('white'), `\n${JSON.stringify(element, Object.getOwnPropertyNames(element), '  ')}\n`); // eslint-disable-line no-console
+          }
+        } catch (e) {
+          console.log(getColor('white'), `\n${element}\n`); // eslint-disable-line no-console
+        }
+      });
+    };
+
+    const printString = (escape, tag, list) => {
+      console.log(escape, tag, new Date().toLocaleString(), '-', list.reduce((ant, at) => `${ant} ${at}`)); // eslint-disable-line no-console
+    };
+
+    const print = (escape, tag, ...args) => {
+      const argsText = args[0].filter(o => typeof o === 'string');
+      const argsObj = args[0].filter(o => typeof o === 'object');
+      if (argsText && argsText.length > 0) printString(getColor(escape), tag, argsText);
+      if (argsObj && argsObj.length > 0) printObjects(argsObj);
+    };
+
+    // loglevel 0
+    this.debug = (...args) => {
+      if (logLevels[settings.logLevel] === 0) {
+        print('blue', '[DBG]', args);
+      }
+    };
+
+    // loglevel 1
+    this.info = (...args) => {
+      if (logLevels[settings.logLevel] <= 1) {
+        print('green', '[INF]', args);
+      }
+    };
+
+    // loglevel 2
+    this.warn = (...args) => {
+      if (logLevels[settings.logLevel] <= 2) {
+        print('yellow', '[WRN]', args);
+      }
+    };
+
+    // loglevel 3
+    this.error = (...args) => {
+      if (logLevels[settings.logLevel] <= 3) {
+        print('red', '[ERR]', args);
+      }
+    };
+
+    this.enableColors = () => {
+      settings.disableColors = false;
+    };
+
+    this.disableColors = () => {
+      settings.disableColors = true;
+    };
+  })(c),
+};
--- postgres.js
+++ postgres.js
@@ -1,145 +1,127 @@
 const pg = require('pg');
 const fs = require('fs');
 const path = require('path');
+const stdout = require('./STDOut');
 
 module.exports = {
-  getInterface: () => config => new (() => {
+  getInterface: c => new (function PostgrSQLInterface(config) {
+    const $this = this;
+    this.config = config;
     const connection = config.connection;
     const baseQueryDir = config.queriesFolder || './queries/';
     const sqlCache = {};
-
-    const displayLog = () => {
-      console.log(Array.prototype.slice.call(arguments));
-    };
-
-    const displayError = () => {
-      console.error(Array.prototype.slice.call(arguments));
-    };
-
-    const displayWarning = () => {
-      console.warn(Array.prototype.slice.call(arguments));
-    };
-
+    const stdOut = stdout.initialize({ logLevel: config.logLevel });
     const pool = new pg.Pool(connection);
+
     pool.on('error', (err, client) => {
-      displayError('idle client error', err.message, err.stack, client);
+      stdOut.error('idle client error', err, client);
     });
 
-    const sanitizeSqlTemplate = (sql) => {
-      return sql; // (sql || "").replace(/(\n)+/g, " ").replace(/(\t)+/g, " ");
-    };
-
-    const readFile = (file, callback) => {
-      if (sqlCache[file]) {
-        callback(sqlCache[file]);
-      } else {
-        fs.readFile(file, 'utf8', (error, data) => {
-          if (!data) {
-            displayWarning('resolucao da query em " + file + " nao retornou conteudo da query');
-            sqlCache[file] = null;
-            callback(null);
-          } else {
-            data = sanitizeSqlTemplate(data);
-            sqlCache[file] = data;
-            callback(data);
-          }
-        });
-      }
-    };
-
-    const resolveQuery = (queryString, callback) => {
-      if (queryString.substring(0, 3) === 'fs:') {
-        readFile(path.join(baseQueryDir, queryString.substring(3)), callback);
-      } else {
-        callback(queryString);
-      }
-    };
-
-    // impl
     const impl = {
-      getSQLQuery: (fname, callback) => {
-        readFile(baseQueryDir + fname, callback);
-      },
-      findAll: (queryPath, params, success_callback, error_callback) => {
-        resolveQuery(queryPath, (queryText) => {
-          const results = [];
-          pool.connect((err, client, done) => {
-            if (err) {
-              displayLog(err);
-              error_callback({ error: 'DATABASE_CONNECTION_ERROR' }, err);
+      sanitizeSqlTemplate: sql => ($this.config.enableSQLSanitizing) ? (sql || '').replace(/(\n)+/g, ' ').replace(/(\t)+/g, ' ') : sql,
+      readFile: file => new Promise((resolve, reject) => {
+        if (sqlCache[file]) {
+          resolve(sqlCache[file]);
+        } else {
+          fs.readFile(file, 'utf8', (error, data) => {
+            if (!data) {
+              stdOut.error(`The query file "${file}" is not found`);
+              sqlCache[file] = null;
+              reject(error);
             } else {
-              const query = client.query(new pg.Query(queryText, params));
-              query.on('row', (row) => {
-                results.push(row);
-              });
-              query.on('end', () => {
-                done();
-                success_callback(results);
-              });
-              query.on('error', (qerr) => {
-                done();
-                error_callback({ error: 'GET_DATA_ERROR', query: query }, qerr);
-                displayError(qerr);
-              });
+              data = impl.sanitizeSqlTemplate(data);
+              sqlCache[file] = data;
+              resolve(data);
             }
           });
-        });
-      },
-      findOne: (queryPath, params, success_callback, error_callback) => {
-        resolveQuery(queryPath, (queryText) => {
+        }
+      }),
+      resolveQuery: queryString => new Promise((resolve, reject) => {
+        if (queryString.match(/^(fs:)(.+\.SQL)$/i)) {
+          const fileName = path.join(baseQueryDir, queryString.substring(3));
+          impl.readFile(fileName)
+            .then(data => resolve(data))
+            .catch(err => reject(err));
+        } else {
+          resolve(queryString);
+        }
+      }),
+      findOne: (queryString, params) => new Promise((resolve, reject) => {
+        impl.resolveQuery(queryString).then((queryText) => {
           let result;
           pool.connect((err, client, done) => {
             if (err) {
-              displayLog(err);
-              error_callback({ error: 'DATABASE_CONNECTION_ERROR' }, err);
-            } else {
-              const query = client.query(new pg.Query(queryText, params));
-              query.on('row', (row) => {
-                result = row;
-              });
-              query.on('end', () => {
-                done();
-                success_callback(result);
-              });
-              query.on('error', (qerr) => {
-                displayError(`DB_ERROR: ${qerr}`);
-                done();
-                error_callback({ error: 'GET_DATA_ERROR' }, err);
-              });
+              stdOut.error(err);
+              return reject(err);
             }
+            const query = client.query(new pg.Query(queryText, params));
+            query.on('row', (row) => {
+              result = row;
+            });
+            query.on('end', () => {
+              done();
+              return resolve(result);
+            });
+            query.on('error', (qerr) => {
+              stdOut.error('DB_ERROR', qerr);
+              done();
+              return reject(err);
+            });
           });
-        });
-      },
-      executeNonQuery: (queryPath, params, success_callback, error_callback) => {
-        resolveQuery(queryPath, (queryText) => {
+        }).catch(err => reject(err));
+      }),
+      findAll: (queryString, params) => new Promise((resolve, reject) => {
+        impl.resolveQuery(queryString).then((queryText) => {
+          const results = [];
           pool.connect((err, client, done) => {
             if (err) {
-              displayLog(err);
-              error_callback({ error: 'DATABASE_CONNECTION_ERROR' }, err);
-            } else {
-              const query = client.query(new pg.Query(queryText, params));
-              query.on('end', (result) => {
-                done();
-                success_callback(result.rowCount);
-              });
-              query.on('error', (qerr) => {
-                done();
-                if (err.code === 23505) {
-                  error_callback({ error: 'UNIQUE_VIOLATION' }, qerr);
-                } else {
-                  error_callback({ error: 'SET_DATA_ERROR' }, err);
-                }
-              });
+              stdOut.error(err);
+              return reject(err);
             }
+            const query = client.query(new pg.Query(queryText, params));
+            query.on('row', (row) => {
+              results.push(row);
+            });
+            query.on('end', () => {
+              done();
+              return resolve(results);
+            });
+            query.on('error', (qerr) => {
+              done();
+              stdOut.error(qerr);
+              return reject(qerr);
+            });
+          });
+        }).catch(err => reject(err));
+      }),
+      executeNonQuery: (queryString, params) => new Promise((resolve, reject) => {
+        impl.resolveQuery(queryString).then((queryText) => {
+          pool.connect((err, client, done) => {
+            if (err) {
+              stdOut.error(err);
+              return reject(err);
+            }
+            const query = client.query(new pg.Query(queryText, params));
+            query.on('end', (result) => {
+              done();
+              return resolve(result.rowCount);
+            });
+            query.on('error', (qerr) => {
+              done();
+              return reject(qerr);
+            });
           });
         });
-      },
+      }),
     };
 
-    // interface
-    this.getSQLQuery = impl.getSQLQuery;
-    this.findAll = impl.findAll;
-    this.findOne = impl.findOne;
-    this.executeNonQuery = impl.executeNonQuery;
-  })(config),
+
+    // public interface
+    this.prepareStatement = async sql => impl.resolveQuery(sql);
+    this.findOne = async (query, queryParams) => impl.findOne(query, queryParams);
+    this.findAll = async (query, queryParams) => impl.findAll(query, queryParams);
+    this.executeNonQuery = async (query, queryParams) => impl.executeNonQuery(query, queryParams);
+
+    stdOut.info('PostgreSQL Client 2.0.0');
+  })(c),
 };
